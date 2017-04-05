1455009165eb8159ba1c4fd2176b8e7077a26d4e
Richard Reeve <git@richardreeve.net>
2017-04-05 12:43:06 +0100

    Add in Renyi divergence docs.

--- src/metrics.jl
+++ src/metrics.jl
@@ -29,6 +29,40 @@ type CorrDist <: SemiMetric end
 type ChiSqDist <: SemiMetric end
 type KLDivergence <: PreMetric end
 
+"""
+    RenyiDivergence(α::Real)
+    renyi_divergence(P, Q, α::Real)
+
+Create a Rényi premetric of order α.
+
+Rényi defined a spectrum of divergence measures generalising the
+Kullback–Leibler divergence (see `KLDivergence`). The divergence is
+not a semimetric as it is not symmetric. It is parameterised by a
+parameter α, and is equal to Kullback–Leibler divergence at α = 1:
+
+At α = 0, ``R_0(P | Q) = -log(sum_{i: p_i > 0}(q_i))``
+
+At α = 1, ``R_1(P | Q) = sum_{i: p_i > 0}(p_i log(p_i / q_i))``
+
+At α = ∞, ``R_∞(P | Q) = log(sup_{i: p_i > 0}(p_i / q_i))``
+
+Otherwise ``R_α(P | Q) = log(sum_{i: p_i > 0}((p_i ^ α) / (q_i ^ (α - 1))) / (α - 1)``
+
+# Example:
+```jldoctest
+julia> x = reshape([0.1, 0.3, 0.4, 0.2], 2, 2);
+
+julia> pairwise(RenyiDivergence(0), x, x)
+2×2 Array{Float64,2}:
+ 0.0  0.0
+ 0.0  0.0
+
+julia> pairwise(Euclidean(2), x, x)
+2×2 Array{Float64,2}:
+ 0.0       0.577315
+ 0.655407  0.0
+```
+"""
 immutable RenyiDivergence{T <: Real} <: PreMetric
     p::T # order of power mean (order of divergence - 1)
     is_normal::Bool
@@ -250,6 +284,9 @@ function eval_end{T<:AbstractFloat}(dist::RenyiDivergence, s::Tuple{T, T, T, T})
         log(s[2]) + log(s[4] / s[3])
     end
 end
+
+# Combine docs with RenyiDivergence
+@doc (@doc RenyiDivergence) renyi_divergence
 
 renyi_divergence(a::AbstractArray, b::AbstractArray, q::Real) = evaluate(RenyiDivergence(q), a, b)
 
