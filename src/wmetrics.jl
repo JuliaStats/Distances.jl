b03378ab47df32b6aca45e06a577461028b9e00f
chethega <chethega@gmail.com>
2017-11-28 17:51:36 +0000

    Elide some more boundchecks.

--- src/wmetrics.jl
+++ src/wmetrics.jl
@@ -45,7 +45,7 @@ end
 function result_type(dist::UnionWeightedMetrics, ::AbstractArray{T1}, ::AbstractArray{T2}) where {T1, T2}
     typeof(evaluate(dist, one(T1), one(T2)))
 end
-function eval_start(d::UnionWeightedMetrics, a::AbstractArray, b::AbstractArray)
+@inline function eval_start(d::UnionWeightedMetrics, a::AbstractArray, b::AbstractArray)
     zero(result_type(d, a, b))
 end
 eval_end(d::UnionWeightedMetrics, s) = s
@@ -62,20 +62,22 @@ eval_end(d::UnionWeightedMetrics, s) = s
     if length(a) == 0
         return zero(result_type(d, a, b))
     end
-    s = eval_start(d, a, b)
-    if size(a) == size(b)
-        @simd for I in eachindex(a, b, d.weights)
-            @inbounds ai = a[I]
-            @inbounds bi = b[I]
-            @inbounds wi = d.weights[I]
-            s = eval_reduce(d, s, eval_op(d, ai, bi, wi))
-        end
-    else
-        for (Ia, Ib, Iw) in zip(eachindex(a), eachindex(b), eachindex(d.weights))
-            @inbounds ai = a[Ia]
-            @inbounds bi = b[Ib]
-            @inbounds wi = d.weights[Iw]
-            s = eval_reduce(d, s, eval_op(d, ai, bi, wi))
+    @inbounds begin
+        s = eval_start(d, a, b)
+        if size(a) == size(b)
+            @simd for I in eachindex(a, b, d.weights)
+                ai = a[I]
+                bi = b[I]
+                wi = d.weights[I]
+                s = eval_reduce(d, s, eval_op(d, ai, bi, wi))
+            end
+        else
+            for (Ia, Ib, Iw) in zip(eachindex(a), eachindex(b), eachindex(d.weights))
+                ai = a[Ia]
+                bi = b[Ib]
+                wi = d.weights[Iw]
+                s = eval_reduce(d, s, eval_op(d, ai, bi, wi))
+            end
         end
     end
     return eval_end(d, s)
