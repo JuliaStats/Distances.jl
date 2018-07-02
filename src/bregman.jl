0663ad92a9d6c991bbca1e14bd9926ca348bcd14
Arnav Sood <soodarnav01@gmail.com>
2018-07-02 10:55:05 -0700

    unindent

--- src/bregman.jl
+++ src/bregman.jl
@@ -1,49 +1,48 @@
 # Bregman divergence 
 
 """
-    Implements the Bregman divergence, a friendly introduction to which can be found [here](http://mark.reid.name/blog/meet-the-bregman-divergences.html). Bregman divergences are a minimal implementation of the "mean-minimizer" property. 
-    
-    It is assumed that the (convex) function F maps vectors (of any type or size) to real numbers. The inner product used is `Base.dot`, but one can be passed in either by defining `inner` or by passing in a keyword argument. If an analytic gradient isn't available, Julia offers a suite of good automatic differentiation packages. 
+Implements the Bregman divergence, a friendly introduction to which can be found
+[here](http://mark.reid.name/blog/meet-the-bregman-divergences.html). 
+Bregman divergences are a minimal implementation of the "mean-minimizer" property. 
 
-    function evaluate(dist::Bregman, p::AbstractVector, q::AbstractVector)
+It is assumed that the (convex differentiable) function F maps vectors (of any type or size) to real numbers. 
+The inner product used is `Base.dot`, but one can be passed in either by defining `inner` or by 
+passing in a keyword argument. If an analytic gradient isn't available, Julia offers a suite 
+of good automatic differentiation packages. 
+
+function evaluate(dist::Bregman, p::AbstractVector, q::AbstractVector)
 """
-struct Bregman <: PreMetric
-    F::Function 
-    ∇::Function
-    inner::Function 
+struct Bregman{T1 <: Function, T2 <: Function, T3 <: Function} <: PreMetric
+    F::T1
+    ∇::T2
+    inner::T3
 end
 
 # Default costructor. 
-Bregman(F, ∇) =  Bregman(F, ∇, Base.dot)
+Bregman(F, ∇) =  Bregman(F, ∇, LinearAlgebra.dot)
 
 # Evaluation fuction 
 function evaluate(dist::Bregman, p::AbstractVector, q::AbstractVector)
-    # Check inputs.
-    F = dist.F;
-    ∇ = dist.∇;
-    inner = dist.inner; 
-    FP_val = F(p);
-    FQ_val = F(q); 
-    DQ_val = ∇(q);
+    # Create cache vals.
+    FP_val = dist.F(p);
+    FQ_val = dist.F(q); 
+    DQ_val = dist.∇(q);
     p_size = size(p)
     # Check F codomain. 
-    if isa(FP_val, Real) && isa(FQ_val, Real) 
-    else 
+    if !(isa(FP_val, Real) && isa(FQ_val, Real))
         throw(ArgumentError("F Codomain Error: F doesn't map the vectors to real numbers"))
     end 
     # Check vector size. 
-    if p_size == size(q) 
-    else
+    if !(p_size == size(q))
         throw(DimensionMismatch("The vector p ($(size(p))) and q ($(size(q))) are different sizes."))
     end
     # Check gradient size. 
-    if size(DQ_val) == p_size
-    else 
+    if !(size(DQ_val) == p_size)
         throw(DimensionMismatch("The gradient result is not the same size as p and q"))
     end 
     # Return the Bregman divergence. 
-    return FP_val - FQ_val - inner(DQ_val, p-q);
+    return FP_val - FQ_val - dist.inner(DQ_val, p-q);
 end 
 
-# Conveniece function. 
-bregman(F, ∇, x, y; inner = Base.dot) = evaluate(Bregman(F, ∇, inner), x, y)
+# Convenience function. 
+bregman(F, ∇, x, y; inner = LinearAlgebra.dot) = evaluate(Bregman(F, ∇, inner), x, y)
