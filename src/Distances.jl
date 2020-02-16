1e88fe458024080e7994549136cf117ab9e08433
Charles Kawczynski <kawczynski.charles@gmail.com>
2020-02-16 15:02:12 -0800

    Add Wasserstein distance

--- src/Distances.jl
+++ src/Distances.jl
@@ -58,6 +58,7 @@ export
     RMSDeviation,
     NormRMSDeviation,
     Bregman,
+    Wasserstein,
 
     # convenient functions
     euclidean,
@@ -91,6 +92,7 @@ export
     bhattacharyya,
     hellinger,
     bregman,
+    wasserstein,
 
     haversine,
 
@@ -107,5 +109,6 @@ include("haversine.jl")
 include("mahalanobis.jl")
 include("bhattacharyya.jl")
 include("bregman.jl")
+include("wasserstein.jl")
 
 end # module end
