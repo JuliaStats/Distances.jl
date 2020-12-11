ebe17a5223c6e7a25e693ffe68cd4b59c7161946
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2020-12-11 17:47:15 +0100

    Update src/generic.jl
    Co-authored-by: Milan Bouchet-Valat <nalimilan@club.fr>

--- src/generic.jl
+++ src/generic.jl
@@ -36,7 +36,6 @@ data `a` and `b`.
 result_type(::PreMetric, ::Type, ::Type) = Float64 # fallback in Distances
 result_type(f, a::Type, b::Type) = typeof(f(oneunit(a), oneunit(b))) # don't require `PreMetric` subtyping
 
-# Promote Arrays and Numbers to types
 result_type(dist, a::AbstractArray, b::AbstractArray) = result_type(dist, eltype(a), eltype(b))
 result_type(dist, a::Number, b::Number) = result_type(dist, typeof(a), typeof(b))
 
