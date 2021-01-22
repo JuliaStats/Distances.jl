9ca4b1d05336709388659229b424b8f6ddafeac2
Daniel Karrasch <daniel.karrasch@posteo.de>
2021-01-22 11:46:49 +0100

    Apply suggestions from code review
    Co-authored-by: Milan Bouchet-Valat <nalimilan@club.fr>

--- src/haversine.jl
+++ src/haversine.jl
@@ -1,8 +1,8 @@
 """
-    Haversine([radius])
+    Haversine(radius=6_371_000)
 
 The haversine distance between two locations on a sphere of given `radius`, whose
-default value is 6371000, i.e., the Earth's (volumetric) mean radius in units of meter; cf.
+default value is 6,371,000, i.e., the Earth's (volumetric) mean radius in meters; cf.
 [NASA's Earth Fact Sheet](https://nssdc.gsfc.nasa.gov/planetary/factsheet/earthfact.html).
 
 Locations are described with longitude and latitude in degrees.
