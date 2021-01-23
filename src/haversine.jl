9b1dc0480644c8367176333fcd3b40373ad91100
Daniel Karrasch <Daniel.Karrasch@gmx.de>
2021-01-23 12:01:08 +0100

    remove haversine workaround

--- src/haversine.jl
+++ src/haversine.jl
@@ -11,34 +11,27 @@ The computed distance has the unit of the radius.
 struct Haversine{T} <: Metric
     radius::T
 end
-Haversine() = Haversine(Float32(6371000))
+Haversine() = Haversine(Float32(6_371_000))
 
 function (dist::Haversine)(x, y)
     length(x) == length(y) == 2 || haversine_error(dist)
 
-    @inbounds x1, x2 = x
-    @inbounds y1, y2 = y
-    # longitudes
-    Δλ = _deg2rad(y1 - x1)
+    @inbounds λ₁, φ₁ = x
+    @inbounds λ₂, φ₂ = y
 
-    # latitudes
-    φ₁ = _deg2rad(x2)
-    φ₂ = _deg2rad(y2)
-    Δφ = φ₂ - φ₁
+    Δλ = λ₂ - λ₁  # longitudes
+    Δφ = φ₂ - φ₁  # latitudes
 
     # haversine formula
-    a = sin(Δφ/2)^2 + cos(φ₁)*cos(φ₂)*sin(Δλ/2)^2
+    a = sind(Δφ/2)^2 + cosd(φ₁)*cosd(φ₂)*sind(Δλ/2)^2
 
     # distance on the sphere
     2 * dist.radius * asin( min(√a, one(a)) ) # take care of floating point errors
 end
 
-haversine(x, y, radius=Float32(6371000)) = Haversine(radius)(x, y)
+haversine(x, y, radius=Float32(6_371_000)) = Haversine(radius)(x, y)
 
 @noinline haversine_error(dist) = throw(ArgumentError("expected both inputs to have length 2 in $dist distance"))
-
-_deg2rad(x::Real) = deg2rad(x)
-_deg2rad(x::Number) = x # targets unitful numbers
 
 """
     SphericalAngle()
