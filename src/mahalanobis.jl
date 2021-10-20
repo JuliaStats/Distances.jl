c960586a12de140e4783c085cc60bca44f78719a
Daniel Karrasch <Daniel.Karrasch@gmx.de>
2021-10-20 19:17:17 +0200

    refactor mahalanobis

--- src/mahalanobis.jl
+++ src/mahalanobis.jl
@@ -74,36 +74,47 @@ end
 
 # SqMahalanobis
 
-function (dist::SqMahalanobis)(a::AbstractVector, b::AbstractVector)
+function (dist::Union{SqMahalanobis,Mahalanobis})(a::AbstractVector, b::AbstractVector)
     if length(a) != length(b)
         throw(DimensionMismatch("first array has length $(length(a)) which does not match the length of the second, $(length(b))."))
     end
 
     Q = dist.qmat
     z = a - b
-    return dot(z, Q * z)
+    return eval_end(dist, dot(z, Q * z))
 end
 
 sqmahalanobis(a::AbstractVector, b::AbstractVector, Q::AbstractMatrix) = SqMahalanobis(Q)(a, b)
+mahalanobis(a::AbstractVector, b::AbstractVector, Q::AbstractMatrix) = Mahalanobis(Q)(a, b)
 
-function colwise!(r::AbstractArray, dist::SqMahalanobis, a::AbstractMatrix, b::AbstractMatrix)
-    Q = dist.qmat
-    get_colwise_dims(size(Q, 1), r, a, b)
-    z = a - b
-    dot_percol!(r, Q * z, z)
-end
-
-function colwise!(r::AbstractArray, dist::SqMahalanobis, a::AbstractVector, b::AbstractMatrix)
+function _colwise!(r, dist, a, b)
     Q = dist.qmat
     get_colwise_dims(size(Q, 1), r, a, b)
     z = a .- b
     dot_percol!(r, Q * z, z)
 end
-function colwise!(r::AbstractArray, dist::SqMahalanobis, a::AbstractMatrix, b::AbstractVector)
-    colwise!(r, dist, b, a)
+
+function colwise!(r::AbstractArray, dist::SqMahalanobis, a::AbstractMatrix, b::AbstractMatrix)
+    _colwise!(r, dist, a, b)
+end
+function colwise!(r::AbstractArray, dist::SqMahalanobis, a::AbstractVector, b::AbstractMatrix)
+    _colwise!(r, dist, a, b)
+end
+function colwise!(r::AbstractArray, dist::Union{SqMahalanobis,Mahalanobis}, a::AbstractMatrix, b::AbstractVector)
+    _colwise!(r, dist, a, b)
 end
 
-function _pairwise!(r::AbstractMatrix, dist::SqMahalanobis, a::AbstractMatrix, b::AbstractMatrix)
+function colwise!(r::AbstractArray, dist::Mahalanobis, a::AbstractMatrix, b::AbstractMatrix)
+    sqrt!(_colwise!(r, dist, a, b))
+end
+function colwise!(r::AbstractArray, dist::Mahalanobis, a::AbstractVector, b::AbstractMatrix)
+    sqrt!(_colwise!(r, dist, a, b))
+end
+function colwise!(r::AbstractArray, dist::Mahalanobis, a::AbstractMatrix, b::AbstractVector)
+    sqrt!(_colwise!(r, dist, a, b))
+end
+
+function _pairwise!(r::AbstractMatrix, dist::Union{SqMahalanobis,Mahalanobis}, a::AbstractMatrix, b::AbstractMatrix)
     Q = dist.qmat
     m, na, nb = get_pairwise_dims(size(Q, 1), r, a, b)
 
@@ -115,13 +126,13 @@ function _pairwise!(r::AbstractMatrix, dist::SqMahalanobis, a::AbstractMatrix, b
 
     for j = 1:nb
         @simd for i = 1:na
-            @inbounds r[i, j] = max(sa2[i] + sb2[j] - 2 * r[i, j], 0)
+            @inbounds r[i, j] = eval_end(dist, max(sa2[i] + sb2[j] - 2 * r[i, j], 0))
         end
     end
     r
 end
 
-function _pairwise!(r::AbstractMatrix, dist::SqMahalanobis, a::AbstractMatrix)
+function _pairwise!(r::AbstractMatrix, dist::Union{SqMahalanobis,Mahalanobis}, a::AbstractMatrix)
     Q = dist.qmat
     m, n = get_pairwise_dims(size(Q, 1), r, a)
 
@@ -135,36 +146,11 @@ function _pairwise!(r::AbstractMatrix, dist::SqMahalanobis, a::AbstractMatrix)
         end
         r[j, j] = 0
         for i = (j + 1):n
-            @inbounds r[i, j] = max(sa2[i] + sa2[j] - 2 * r[i, j], 0)
+            @inbounds r[i, j] = eval_end(dist, max(sa2[i] + sa2[j] - 2 * r[i, j], 0))
         end
     end
     r
 end
 
-
-# Mahalanobis
-
-function (dist::Mahalanobis)(a::AbstractVector, b::AbstractVector)
-    sqrt(SqMahalanobis(dist.qmat, skipchecks = true)(a, b))
-end
-
-mahalanobis(a::AbstractVector, b::AbstractVector, Q::AbstractMatrix) = Mahalanobis(Q)(a, b)
-
-function colwise!(r::AbstractArray, dist::Mahalanobis, a::AbstractMatrix, b::AbstractMatrix)
-    sqrt!(colwise!(r, SqMahalanobis(dist.qmat, skipchecks = true), a, b))
-end
-
-function colwise!(r::AbstractArray, dist::Mahalanobis, a::AbstractVector, b::AbstractMatrix)
-    sqrt!(colwise!(r, SqMahalanobis(dist.qmat, skipchecks = true), a, b))
-end
-function colwise!(r::AbstractArray, dist::Mahalanobis, a::AbstractMatrix, b::AbstractVector)
-    sqrt!(colwise!(r, SqMahalanobis(dist.qmat, skipchecks = true), a, b))
-end
-
-function _pairwise!(r::AbstractMatrix, dist::Mahalanobis, a::AbstractMatrix, b::AbstractMatrix)
-    sqrt!(_pairwise!(r, SqMahalanobis(dist.qmat, skipchecks = true), a, b))
-end
-
-function _pairwise!(r::AbstractMatrix, dist::Mahalanobis, a::AbstractMatrix)
-    sqrt!(_pairwise!(r, SqMahalanobis(dist.qmat, skipchecks = true), a))
-end
+eval_end(::SqMahalanobis, x) = x
+eval_end(::Mahalanobis, x) = sqrt(x)
