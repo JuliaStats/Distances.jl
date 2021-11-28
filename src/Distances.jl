7b6721beb49a6edc4810ac59541396fcf536ab3e
John Lapeyre <jlapeyre@users.noreply.github.com>
2021-11-28 14:41:21 -0500

    Delete trailing whitespace 	modified:   src/Distances.jl 	modified:   src/bregman.jl

--- src/Distances.jl
+++ src/Distances.jl
@@ -102,8 +102,8 @@ export
 
 if VERSION < v"1.2-"
     import Base: has_offset_axes
-    require_one_based_indexing(A...) = 
-        !has_offset_axes(A...) || 
+    require_one_based_indexing(A...) =
+        !has_offset_axes(A...) ||
             throw(ArgumentError("offset arrays are not supported but got an array with index other than 1"))
 else
     import Base: require_one_based_indexing
