559e6d132863aa057e662b81ae6248daa220b7f4
David Widmann <devmotion@users.noreply.github.com>
2023-10-06 00:08:00 +0200

    Update test/runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,6 +13,9 @@ include("F64.jl")
 include("test_dists.jl")
 
 # Test ChainRules definitions on Julia versions that support weak dependencies
-if isdefined(Base, :get_extension)
+# Support for extensions was added in
+# https://github.com/JuliaLang/julia/commit/93587d7c1015efcd4c5184e9c42684382f1f9ab2
+# https://github.com/JuliaLang/julia/pull/47695 
+if VERSION >= v"1.9.0-alpha1.18"
     include("chainrules.jl")
 end
