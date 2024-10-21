39ea853b9eeed2cca38a072380bfb2a3102670b6
Elias Carvalho <eliascarvdev@gmail.com>
2024-10-21 13:46:47 -0300

    Apply suggestions

--- src/metrics.jl
+++ src/metrics.jl
@@ -12,12 +12,27 @@ abstract type UnionSemiMetric <: SemiMetric end
 
 abstract type UnionMetric <: Metric end
 
-# a minkowski metric is a metric that is defined by the formula:
-#
-#   d(x, y) = sum(w .* (x - y) .^ p) ^ (1 / p)
-#
-# where the `p` parameter defines the metric 
-# and `w` is a potential weight vector (all 1's by default).
+"""
+    MinkowskiMetric
+
+A Minkowski metric is a metric that is defined by the formula:
+
+`d(x, y) = sum(w .* (x - y) .^ p) ^ (1 / p)`
+
+where the `p` parameter defines the metric 
+and `w` is a potential weight vector (all 1's by default).
+
+Common Minkowski metrics:
+* `Cityblock`/`WeightedCityblock`: Minkowski metric with `p=1`;
+* `Euclidean`/`WeightedEuclidean`: Minkowski metric with `p=2`;
+* `Chebyshev`: Limit of `d` as `p` approaches infinity;
+* `Minkowski`/`WeightedMinkowski`: generic Minkowski metric for any `p`.
+
+## Notes
+* The difference between `Minkowski`/`WeightedMinkowski` and `MinkowskiMetric` 
+  is that while the first are generic Minkowski metrics, 
+  the second is the parent type of these metrics.
+"""
 abstract type MinkowskiMetric <: UnionMetric end
 
 ###########################################################
